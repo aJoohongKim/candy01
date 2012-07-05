12b79f2f34f117b75f6c32c04f66762044eee662
Joohong Kim <joohong.kim@access-company.com>
2012-07-05 20:14:19 +0900

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Candy01::Application.config.secret_token = '08f8acd15681a684128b83ecf385de796d1332ab581e5a0d33887e8ae11a374179b76fa8a2d5748e39f9beefa4b9361a54107037d877dde55c68218e2f36bbe6'
